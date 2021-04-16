31ba8981418dbb8c24309a5aecdcc0f2dafe7263
Nishanth Gaddam <57975222+nishanth37@users.noreply.github.com>
2021-04-16 12:22:51 -0500

    Time: 0 ms (100.00%), Space: 6.6 MB (35.27%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -4,24 +4,29 @@ public:
         // edge case
         if(digits.size() == 0) return {};
         // hashmap
-        unordered_map<char, vector<string>> map;
-        map['2'] = {"a", "b", "c"};
-        map['3'] = {"d", "e", "f"};
-        map['4'] = {"g", "h", "i"};
-        map['5'] = {"j", "k", "l"};
-        map['6'] = {"m", "n", "o"};
-        map['7'] = {"p", "q", "r", "s"};
-        map['8'] = {"t", "u", "v"};
-        map['9'] = {"w", "x", "y", "z"};
-        vector<string> res = map[digits[0]];
+        unordered_map<char, string> map;
+        map['2'] = "abc";
+        map['3'] = "def";
+        map['4'] = "ghi";
+        map['5'] = "jkl";
+        map['6'] = "mno";
+        map['7'] = "pqrs";
+        map['8'] = "tuv";
+        map['9'] = "wxyz";
+        vector<string> res;
+        for(int i = 0; i < map[digits[0]].size(); i++) {
+            string str;
+            str.append(1, map[digits[0]][i]);
+            res.push_back(str);
+        }  
         vector<string> temp;
         for(int i = 1; i < digits.size(); i++) {
             temp = res;
             res.clear();
             for(int j = 0; j < temp.size(); j++) {
-                vector<string> arr = map[digits[i]];
-                for(int k = 0; k < arr.size(); k++) {
-                    res.push_back(temp[j] + arr[k]);
+                string str = map[digits[i]];
+                for(int k = 0; k < str.size(); k++) {
+                    res.push_back(temp[j] + str[k]);
                 }
             }
         }
