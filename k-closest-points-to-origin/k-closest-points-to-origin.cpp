c5a60621ef3a81ad7e710370c9c1922e3b85c89c
Nishanth Gaddam <57975222+nishanth37@users.noreply.github.com>
2021-06-01 13:16:54 -0500

    Time: 316 ms (14.77%), Space: 66.2 MB (25.22%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,17 +1,14 @@
-// Using MaxHeap
-// Time Complexity: O(NlogK) N = #points, logK for heapify
-// Space Complexity: O(K)
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
-        priority_queue<vector<int>, vector<vector<int>>, compareDist> pq; // space: O(K)
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<vector<int>, vector<vector<int>>, compareDist> pq;
         vector<vector<int>> res;
-        for(auto point : points) { // time: O(N)
+        for(auto point : points) {
             pq.push(point);
-            if(pq.size() > K)
+            if(pq.size() > k)
                 pq.pop();
         }
-        while(!pq.empty()) { //time: O(K)
+        while(!pq.empty()) {
             res.push_back(pq.top());
             pq.pop();
         }
@@ -19,8 +16,8 @@ public:
     }
 private:
     struct compareDist {
-        bool operator()(vector<int>& p, vector<int>& q) {
-            return p[0]*p[0] + p[1]*p[1] < q[0]*q[0] + q[1]*q[1];
+        bool operator()(vector<int> &p, vector<int> &q) {
+            return (p[0]*p[0] + p[1]*p[1] < q[0]*q[0] + q[1]*q[1]);
         }
     };
 };
