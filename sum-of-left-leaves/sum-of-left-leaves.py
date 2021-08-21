fe4e98ce7aab5840e1c7f53bbf4122e82b1ce1b7
Nishanth Gaddam <57975222+nishanth37@users.noreply.github.com>
2021-08-21 14:53:38 -0500

    Time: 24 ms (97.74%), Space: 14.6 MB (77.87%) - LeetHub

---
+++ sum-of-left-leaves/sum-of-left-leaves.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    def sumOfLeftLeaves(self, root):
+        total_sum = 0
+        current_node = root
+        while current_node is not None:
+            # If there is no left child, we can simply explore the right subtree
+            # without needing to worry about keeping track of currentNode's other
+            # child.
+            if current_node.left is None: 
+                current_node = current_node.right 
+            else: 
+                previous = current_node.left 
+                # Check if this left node is a leaf node.
+                if previous.left is None and previous.right is None:
+                    total_sum += previous.val
+                # Find the inorder predecessor for currentNode.
+                while previous.right is not None and previous.right is not current_node:
+                    previous = previous.right
+                # We've not yet visited the inorder predecessor. This means that we 
+                # still need to explore currentNode's left subtree. Before doing this,
+                # we will put a link back so that we can get back to the right subtree
+                # when we need to.
+                if previous.right is None:
+                    previous.right = current_node  
+                    current_node = current_node.left  
+                # We have already visited the inorder predecessor. This means that we
+                # need to remove the link we added, and then move onto the right
+                # subtree and explore it.
+                else:
+                    previous.right = None
+                    current_node = current_node.right
+        return total_sum        
