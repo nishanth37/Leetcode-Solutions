5c704457d089c1895c7dc6386a4948863287e305
Nishanth Gaddam <57975222+nishanth37@users.noreply.github.com>
2025-07-25 12:59:39 -0500

    Time: 7 ms (3.13%), Space: 42.8 MB (20%) - LeetHub

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSum(int[] nums) {
+        Set<Integer> positiveNumsSet = new HashSet<Integer>();
+        for(int num: nums) {
+            if(num > 0)
+                positiveNumsSet.add(num);
+        }
+        if (positiveNumsSet.isEmpty())
+            return Arrays.stream(nums).max().getAsInt();
+        return positiveNumsSet.stream().mapToInt(Integer::intValue).sum();
+    }
+}
