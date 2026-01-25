84d32102edd54962fe07c0fbc446d1604fbfc5cd
Nishanth Gaddam <57975222+nishanth37@users.noreply.github.com>
2026-01-25 14:24:24 -0600

    Time: 1 ms (97.06%), Space: 42.1 MB (53.81%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,16 @@
+// T.C.: O(sqrt(n))
+// S.C.: O(1)
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum = 0;
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i * i != num) {
+                    sum += num / i;
+                }
+            }
+        }
+        return (sum - num == num);
+    }
+}
